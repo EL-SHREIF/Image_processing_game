196843f9d3f797f089183b63ea6bc5cd7b3fd61b
EL-SHREIF <36492402+EL-SHREIF@users.noreply.github.com>
2020-04-20 09:01:18 +0200

    Make new Gui with basic options in Unity IDE

---
+++ Assets/scripts/script_to_move_between_senes.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class script_to_move_between_senes : MonoBehaviour
+{
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ON_move_click(){
+        SceneManager.LoadScene(1);
+    }
+}
