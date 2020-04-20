dea561afbdddcfc0fc157ec56015a9e48efeebec
EL-SHREIF <36492402+EL-SHREIF@users.noreply.github.com>
2020-04-20 14:20:35 +0200

    Change minor parts in the game scene

--- Assets/scripts/CameraController.cs
+++ Assets/scripts/CameraController.cs
@@ -1,18 +1,67 @@
-﻿using System.Collections;
+﻿using UnityEngine;
 using System.Collections.Generic;
-using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Numerics;
 
 public class CameraController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    private bool camAvl;
+    public WebCamTexture backCam;
+    private Texture d_backGround;
+
+
+
+    public RawImage background;
+    public AspectRatioFitter fit;
+
+
+    private void Start() {
+        d_backGround = background.texture;
+        WebCamDevice[] devices = WebCamTexture.devices;
+
+        if (devices.Length == 0) {
+            Debug.Log("No devices");
+            camAvl = false;
+            return;
+        }
+
+        for (int i = 0; i < devices.Length; i++) {
+            if (devices[i].isFrontFacing) {
+                backCam = new WebCamTexture(devices[i].name, Screen.width, Screen.height);
+            }
+        }
+
+        if (backCam == null) {
+            Debug.Log("unable bardo");
+            return;
+        }
+
+        backCam.Play();
+        background.texture = backCam;
+
+        camAvl = true;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void Update() {
+
+        if (!camAvl)
+            return;
+
+        float ratio = (float)backCam.width / (float)backCam.height;
+        fit.aspectRatio = ratio;
+
+
+        float scaleY = backCam.videoVerticallyMirrored ? -1f : 1f;
+        background.rectTransform.localScale = new UnityEngine.Vector3(1f, scaleY, 1f);
+
+        int orient = -backCam.videoRotationAngle;
+        background.rectTransform.localEulerAngles = new UnityEngine.Vector3(0, 0, orient);
+
+  
     }
+
+
+
+    
 }
