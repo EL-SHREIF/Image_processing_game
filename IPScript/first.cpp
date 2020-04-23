e985c5895f01eb6b3a8e2be247f85a0624f613b1
omarahmed1111 <30423288+omarahmed1111@users.noreply.github.com>
2020-04-23 09:20:29 +0200

    Detect red color and visualize the red color mask

--- IPScript/first.cpp
+++ IPScript/first.cpp
@@ -7,44 +7,6 @@
 using namespace std;
 using namespace cv;
 
-// Declare structure to be used to pass data from C++ to Mono.
-struct Circle
-{
-	Circle(int x, int y, int radius) : X(x), Y(y), Radius(radius) {}
-	int X, Y, Radius;
-};
-
-CascadeClassifier _faceCascade;
-String _windowName = "Unity OpenCV Interop Sample";
-VideoCapture _capture;
-int _scale = 1;
-
-extern "C" int __declspec(dllexport) __stdcall  Init(int& outCameraWidth, int& outCameraHeight)
-{
-	// Load LBP face cascade.
-	if (!_faceCascade.load("lbpcascade_frontalface.xml"))
-		return -1;
-
-	// Open the stream.
-	_capture.open(0);
-	if (!_capture.isOpened())
-		return -2;
-
-	outCameraWidth = _capture.get(CAP_PROP_FRAME_WIDTH);
-	outCameraHeight = _capture.get(CAP_PROP_FRAME_HEIGHT);
-
-	return 0;
-}
-
-extern "C" void __declspec(dllexport) __stdcall  Close()
-{
-	_capture.release();
-}
-
-extern "C" void __declspec(dllexport) __stdcall SetScale(int scale)
-{
-	_scale = scale;
-}
 struct Color32
 {
 	uchar r;
@@ -52,18 +14,22 @@ struct Color32
 	uchar b;
 	uchar a;
 };
-extern "C" int __declspec(dllexport) __stdcall Detect(Color32* img,int width,int height)
-{
-	Mat Img1(height, width, CV_8UC4, img);
-	Mat mask1;
-	// Creating masks to detect the upper and lower red color.
-	inRange(Img1, Scalar(0, 0, 100), Scalar(50, 50, 255), mask1);
+extern "C" int __declspec(dllexport) __stdcall Detect(Color32** img,int width,int height)
+{	
+	Mat Img1(height, width, CV_8UC4, *img);
+	flip(Img1, Img1, -1);
+	imshow("Original Image", Img1);
 	
-	// Generating the final mask
-	mask1 = mask1;
-	int redPixels = 0;
-	for (int i = 0; i < mask1.rows; i++)for (int j = 0; j < mask1.cols; j++)if (mask1.at<cv::Vec3b>(i, j)[0] != 0)redPixels = redPixels + 1;
-	
-	return redPixels;
+	Mat hsv1,hsv2;
+	cvtColor(Img1, hsv1, COLOR_RGBA2BGR);
+	cvtColor(hsv1, hsv2, COLOR_BGR2HSV);
+	Mat mask1, mask2;
+	inRange(hsv2, Scalar(0, 120, 70), Scalar(10, 255, 255), mask1);
+	inRange(hsv2, Scalar(170, 120, 70), Scalar(180, 255, 255), mask2);
+	mask1 = mask1 + mask2;
+	imshow("Mask", mask1);
+
+
+	return 0;
 	
 }
