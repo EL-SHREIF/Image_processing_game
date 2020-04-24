2cf1298a1b03861b52e34443e2482a69b489930a
omarahmed1111 <30423288+omarahmed1111@users.noreply.github.com>
2020-04-24 15:39:29 +0200

    update opencv code

--- IPScript/first.cpp
+++ IPScript/first.cpp
@@ -14,12 +14,10 @@ struct Color32
 	uchar b;
 	uchar a;
 };
-extern "C" int __declspec(dllexport) __stdcall Detect(Color32** img,int width,int height)
+extern "C" float __declspec(dllexport) __stdcall DetectRed(Color32** img,int width,int height)
 {	
 	Mat Img1(height, width, CV_8UC4, *img);
 	flip(Img1, Img1, -1);
-	imshow("Original Image", Img1);
-	
 	Mat hsv1,hsv2;
 	cvtColor(Img1, hsv1, COLOR_RGBA2BGR);
 	cvtColor(hsv1, hsv2, COLOR_BGR2HSV);
@@ -27,9 +25,12 @@ extern "C" int __declspec(dllexport) __stdcall Detect(Color32** img,int width,in
 	inRange(hsv2, Scalar(0, 120, 70), Scalar(10, 255, 255), mask1);
 	inRange(hsv2, Scalar(170, 120, 70), Scalar(180, 255, 255), mask2);
 	mask1 = mask1 + mask2;
-	imshow("Mask", mask1);
-
-
-	return 0;
+	int redPixels = 0;
+	for (int i = 0; i < mask1.rows; i++) {
+		for (int j = 0; j < mask1.cols; j++)if (mask1.at<cv::Vec3b>(i, j)[0] != 0&& mask1.at<cv::Vec3b>(i, j)[1] != 0&& mask1.at<cv::Vec3b>(i, j)[2] != 0)redPixels++;
+	}
+	float score = (float)redPixels / (width*height);
+	score = score * 10;
+	return score;
 	
 }
